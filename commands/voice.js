f376dc887dd32d115844efb6bbfabfd2273c5e7f
Daniel Gallegos <daniel@tacowolf.net>
2015-12-30 00:36:09 -0500

    Fix !voice

--- commands/voice.js
+++ commands/voice.js
@@ -4,7 +4,7 @@ function voice(bot, user, userID, channelID, message) {
     helpers.statistics("voice", user, userID, channelID, message);
     bot.sendMessage({
         to: channelID,
-        message: "Hey, @everyone! **" + user + "** wants to check and see who wants to wants join voice chat."
+        message: "Hey, @everyone ! **" + user + "** wants to check and see who wants to wants join voice chat."
     });
 }
 module.exports = {
