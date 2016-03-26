60a91fdd438e087bc689dd82e00a764e1f47c9b2
Daniel Gallegos <daniel@tacowolf.net>
2016-03-25 22:38:43 -0500

    Add 8ball function

---
+++ commands/8ball.js
@@ -0,0 +1,38 @@
+var helpers = require('./../helpers');
+
+function eightball(bot, user, userID, channelID, message) {
+    helpers.statistics("8ball", user, userID, channelID, message);
+    answers = ['It is certain~',
+        'It is decidedly so. >w>',
+        'Without a doubt~',
+        'Yes, definitely. ^w^',
+        'You may rely on it. >w>',
+        'As I see it, yes! ^w^',
+        'Most likely. :3',
+        'Outlook good~! ^w^',
+        'Yes. :3',
+        'Signs point to yes~ >w<',
+        'Reply hazy... try again. =w=',
+        'Ask again later...',
+        '...better not tell you now... ;w;',
+        'Cannot predict now. -w-',
+        'Concentrate and ask again. -w-',
+        '...don\'t count on it. ;w;',
+        'My reply is no. =w=',
+        'Outlook... not so good. ;w;',
+        'Very doubtful. =w='
+    ]
+    answer = answers[Math.floor(Math.random() * answers.length)];
+    rawQuestion = helpers.parameters(message);
+    rawQuestion.shift();
+    question = helpers.join(rawQuestion);
+    msg = ':grey_question: **Question:** `' + question + '`\n:crystal_ball: **Answer:** `' + answer + '`'
+    bot.sendMessage({
+        to: channelID,
+        message: msg
+    });
+};
+
+module.exports = {
+    command: eightball
+}
