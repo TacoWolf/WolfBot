309f1cdd360ccc3fbcc159fcd455c79df09babf0
Daniel Gallegos <daniel@tacowolf.net>
2016-04-03 12:50:30 -0500

    Fix mentions for 8ball #14

--- commands/8ball.js
+++ commands/8ball.js
@@ -2,29 +2,35 @@ var helpers = require('./../helpers');
 function eightball(bot, user, userID, channelID, message) {
   helpers.statistics('8ball', user, userID, channelID, message);
   answers = [
-    'It is certain~',
-    'It is decidedly so. >w>',
-    'Without a doubt~',
-    'Yes, definitely. ^w^',
-    'You may rely on it. >w>',
-    'As I see it, yes! ^w^',
-    'Most likely. :3',
-    'Outlook good~! ^w^',
-    'Yes. :3',
-    'Signs point to yes~ >w<',
-    'Reply hazy... try again. =w=',
-    'Ask again later...',
-    '...better not tell you now... ;w;',
-    'Cannot predict now. -w-',
-    'Concentrate and ask again. -w-',
-    '...don\'t count on it. ;w;',
-    'My reply is no. =w=',
-    'Outlook... not so good. ;w;',
-    'Very doubtful. =w='
+  'It is certain~',
+  'It is decidedly so. >w>',
+  'Without a doubt~',
+  'Yes, definitely. ^w^',
+  'You may rely on it. >w>',
+  'As I see it, yes! ^w^',
+  'Most likely. :3',
+  'Outlook good~! ^w^',
+  'Yes. :3',
+  'Signs point to yes~ >w<',
+  'Reply hazy... try again. =w=',
+  'Ask again later...',
+  '...better not tell you now... ;w;',
+  'Cannot predict now. -w-',
+  'Concentrate and ask again. -w-',
+  '...don\'t count on it. ;w;',
+  'My reply is no. =w=',
+  'Outlook... not so good. ;w;',
+  'Very doubtful. =w='
   ];
-  answer = answers[Math.floor(Math.random() * answers.length)];
+  answer = helpers.randomArray(answers);
   rawQuestion = helpers.parameters(message);
   rawQuestion.shift();
+  for (var i = rawQuestion.length - 1; i >= 0; i--) {
+    if (/<@.*>/.test(rawQuestion[i])) {
+      rawQuestion[i] = bot.fixMessage(rawQuestion[i]);
+      rawQuestion[i] = '[' + rawQuestion[i] + ']'
+    }
+  }
   question = helpers.join(rawQuestion);
   msg = ':grey_question: **Question:** `' + question + '`\n:crystal_ball: **Answer:** `' + answer + '`';
   bot.sendMessage({
