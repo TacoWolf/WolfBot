bb2087e8a168972dbc02d2fc812c2edf684ec9d6
Daniel Gallegos <thattacoguy@users.noreply.github.com>
2017-03-10 17:50:59 -0500

    Add missing space in error message

--- scripts/pokemon.js
+++ scripts/pokemon.js
@@ -80,7 +80,7 @@ function pokefunction(event) {
       return true;
     }
     msg = '...I-I couldn\'t find a pokémon with information matching your request.';
-    msg += 'I\'m sorry... ;w;';
+    msg += ' I\'m sorry... ;w;';
     event.bot.sendMessage({
       to: event.channelID,
       message: msg,
